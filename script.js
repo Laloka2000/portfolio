a717868e98b0efb30efd9737bdd37c0577a5d51e
Laloka2000 <laszlocodingonly@gmail.com>
2024-07-13 14:01:43 +0200

    Szöveg módosítások

--- script.js
+++ script.js
@@ -34,3 +34,23 @@ function stickyNav (){
   }
 }
 
+//Oldal tetejére gomb
+
+let backtotop = document.getElementById("#felulre");
+
+windows.onscroll = function() {
+  scrollFunction();
+}
+
+function scrollFunction(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    backtotop.style.display = "block";
+  } else {
+    backtotop.style.display = "none";
+  }
+}
+
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
