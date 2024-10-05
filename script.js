33a00a6ee4084a714cea655644a5ccf1ba133a87
Laloka2000 <laszlocodingonly@gmail.com>
2024-10-05 16:52:38 +0200

    Image animation

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-  
-window.addEventListener("scroll", reveal);
 
-
-windows.onscroll = function (){
-  stickyNav();
-}
-
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function stickyNav (){
-  if(windows.pageYOffset >= sticky){
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 //Oldal tetejére gomb
 
