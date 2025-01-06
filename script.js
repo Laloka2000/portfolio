26acf9e0d52f99d9717bb69021d572aa1ab48839
Lelovics László <99196324+Laloka2000@users.noreply.github.com>
2025-01-06 20:39:41 +0100

    Files organised and CV downloader button done

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -14,60 +16,65 @@ function scrollFunction() {
 
 function topFunction() {
   document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
 
 // TypeWriter function
 
 var currentFileName = window.location.pathname.split("/").pop();
 
-var currentLanguage = currentFileName === 'english-version.html' ? 'en' : 'hu';
+var currentLanguage = currentFileName === "english-version.html" ? "en" : "hu";
 
 var textArrayEn = ["Webdeveloper", "Frontend developer", "Coder", "Programmer"];
-var textArrayHu = ["Webfejlesztő", "Frontend Fejlesztő", "Informatikus", "Programozó"];
+var textArrayHu = [
+  "Webfejlesztő",
+  "Frontend Fejlesztő",
+  "Informatikus",
+  "Programozó",
+];
 
-var textArray = currentLanguage === 'en' ? textArrayEn : textArrayHu;
+var textArray = currentLanguage === "en" ? textArrayEn : textArrayHu;
 
-var typeWriterElement = document.getElementById('typewriter');
+var typeWriterElement = document.getElementById("typewriter");
 
 function delWriter(text, i, cb) {
-    if (i >= 0) {
-        typeWriterElement.innerHTML = text.substring(0, i--);
-        var rndBack = 10 + Math.random() * 100;
-        setTimeout(function () {
-            delWriter(text, i, cb);
-        }, rndBack);
-    } else if (typeof cb == 'function') {
-        setTimeout(cb, 1000);
-    }
+  if (i >= 0) {
+    typeWriterElement.innerHTML = text.substring(0, i--);
+    var rndBack = 10 + Math.random() * 100;
+    setTimeout(function () {
+      delWriter(text, i, cb);
+    }, rndBack);
+  } else if (typeof cb == "function") {
+    setTimeout(cb, 1000);
+  }
 }
 
 function typeWriter(text, i, cb) {
-    if (i < text.length + 1) {
-        typeWriterElement.innerHTML = text.substring(0, i++);
-        var rndTyping = 250 - Math.random() * 100;
-        setTimeout(function () {
-            typeWriter(text, i, cb);
-        }, rndTyping);
-    } else if (i === text.length + 1) {
-        setTimeout(function () {
-            delWriter(text, i, cb);
-        }, 1000);
-    }
+  if (i < text.length + 1) {
+    typeWriterElement.innerHTML = text.substring(0, i++);
+    var rndTyping = 250 - Math.random() * 100;
+    setTimeout(function () {
+      typeWriter(text, i, cb);
+    }, rndTyping);
+  } else if (i === text.length + 1) {
+    setTimeout(function () {
+      delWriter(text, i, cb);
+    }, 1000);
+  }
 }
 
 function StartWriter(i) {
-    if (typeof textArray[i] == "undefined") {
-        setTimeout(function () {
-            StartWriter(0);
-        }, 1000);
-    } else {
-        typeWriter(textArray[i], 0, function () {
-            StartWriter(i + 1);
-        });
-    }
+  if (typeof textArray[i] == "undefined") {
+    setTimeout(function () {
+      StartWriter(0);
+    }, 1000);
+  } else {
+    typeWriter(textArray[i], 0, function () {
+      StartWriter(i + 1);
+    });
+  }
 }
 
 setTimeout(function () {
-    StartWriter(0);
+  StartWriter(0);
 }, 1000);
